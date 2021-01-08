e7015d799008675f5d34cddb4d7ff3a5abf581b8
Wilson <karaewilson16@gmail.com>
2021-01-08 16:30:48 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,15 +1,17 @@
 function callMe() {
-  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
+  var lyric = "maybe";
 }
 
-function thisIsCrazy() {
-    console.log("hey!!!")
-    var lyric = "hey!!!"
+
+var lyric = "hey!!!"
+function thisIsCrazy (){
+    console.log(lyric)
+  }
 }
 
 function sayMyName() {
